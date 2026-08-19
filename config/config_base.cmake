9b6835f193f571e053002d13442842d12ffbe129
Sunny CHEN <sunny.chen@st.com>
2026-08-19 11:56:51 +0800

    change TFM_EXTRAS_GIT_REMOTE to github link

--- config/config_base.cmake
+++ config/config_base.cmake
@@ -46,7 +46,8 @@ set(MCUBOOT_PATCH_DIR                   "${CMAKE_SOURCE_DIR}/lib/ext/mcuboot"   
 set(MCUBOOT_FORCE_PATCH                 OFF         CACHE BOOL      "Always apply MCUboot patches")
 
 set(TFM_EXTRAS_REPO_PATH                "DOWNLOAD"  CACHE PATH      "Path to tf-m-extras repo (or DOWNLOAD to fetch automatically")
-set(TFM_EXTRAS_GIT_REMOTE               "https://git.trustedfirmware.org/TF-M/tf-m-extras.git" CACHE STRING "The URL to retrieve tf-m-extras from")
+set(TFM_EXTRAS_GIT_REMOTE               "https://github.com/TrustedFirmware-M/tf-m-extras.git" CACHE STRING "The URL to retrieve tf-m-extras from")
+#set(TFM_EXTRAS_GIT_REMOTE               "https://git.trustedfirmware.org/TF-M/tf-m-extras.git" CACHE STRING "The URL to retrieve tf-m-extras from")
 set(TFM_EXTRAS_REPO_VERSION             "2ac90aff"  CACHE STRING    "The version of tf-m-extras to use")
 
 set(LIB_EVENTLOG_PATH                   "DOWNLOAD"  CACHE PATH      "Path to LibEventLog repo (or DOWNLOAD to fetch automatically")
